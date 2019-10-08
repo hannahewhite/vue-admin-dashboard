28303275b7c50303ee026d3670b7ab691fff1401
Hannah White <hwhite@deputy.com>
2019-10-08 12:38:46 +1100

    Signin & out

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as netlifyIdentityWidget from "netlify-identity-widget";
+import GoTrue from "gotrue-js";
+
 
 Vue.config.productionTip = false;
+
+// Initialise Netlify Identity Widget
+netlifyIdentityWidget.init();
+
+// Initialise GoTrue JS for Netlify
+export const auth = new GoTrue({
+  APIUrl: "https://vue-hq-hannah.netlify.com/.netlify/identity",
+  setCookie: true
+})
 
 new Vue({
   router,
--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -3,22 +3,29 @@
     class="container"
     :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
   >
+    <Notification v-if="hasText" :text="text" />
     <RequestAccount />
     <div class="login">
       <img v-show="isDarkMode" src="@/assets/DCHQ.svg" />
       <img v-show="!isDarkMode" src="@/assets/DCHQ-dark.svg" />
       <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into Design+Code HQ</h4>
+      <form @submit.prevent="onSubmit">
       <input
         :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
         type="email"
         placeholder="Email"
+        v-model="email"
+        required
       />
       <input
         :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
         type="password"
         placeholder="Password"
+        v-model="password"
+        required
       />
       <button>Sign In</button>
+      </form>
       <router-link
         :class="{'light-link' : isDarkMode, 'dark-link' : !isDarkMode}"
         to="/recover"
@@ -29,13 +36,23 @@
 </template>
 
 <script>
+import { auth } from "@/main";
+import Notification from "@/components/Notification";
 import RequestAccount from "@/components/RequestAccount";
 import ThemeSwitch from "@/components/ThemeSwitch";
 
 export default {
   name: "SignIn",
   components: {
-    RequestAccount, ThemeSwitch
+    RequestAccount, ThemeSwitch, Notification
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: ""
+    }
   },
   computed: {
     isDarkMode() {
@@ -45,6 +62,25 @@ export default {
   methods: {
     toggleDarkMode() {
     this.$store.commit("toggleDarkMode");
+    },
+    onSubmit() {
+      const email = this.email;
+      const password = this.password;
+
+      auth.login(email, password, true).then(response => {
+        this.$router.replace("/")
+      }).catch(error => {
+        alert("Error:" + error);
+      });
+    }
+  },
+  
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
     }
   }
 };
@@ -61,6 +97,7 @@ export default {
 
 .login {
   width: 400px;
+  text-align: center;
 }
 
 .request {
